279a973813fb31dcc29ccda3e255c825e5ea9594
Subs <64913834+Subsical@users.noreply.github.com>
2021-05-14 22:13:48 +0100

    yes
    yes

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -1,12 +1,14 @@
 
+fdghdrfhgryghrsdhghgsdfg
 for num = 1, 100 do
     if num % 15 == 0 then
         print("fizzbuzz")
     elseif num % 3 == 0 then
-        print("fizz")
+        print("fizz")rtgdhtfdrgh
     elseif num % 5 == 0 then
         print("buzz")
     else
         print(num)
-    end
+    endfdgfgredgdfg
 end
+fdgfgrgrfdgregdrfgrgredsg
